189eaff53bb82efc5e744e58e36cea39ea5c2606
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-03-28 18:32:25 +0300

    Rebuild modal and JS for it

--- src/main.js
+++ src/main.js
@@ -3,8 +3,5 @@ import './js/up-button';
 import './js/anchor-scroll' ;
 
 import './js/mobile-menu';
-import './js/modal-explore';
-import './js/modal-buy';
-import './js/modal-feedback';
-import './js/modal-review';
-import './js/modal-thank';
+
+import './js/modal';
