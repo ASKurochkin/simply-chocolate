b8cfe3ca9a4717ef4c5e630571e5a3a09d59a62e
happyuser1234 <120355016+happyuser1234@users.noreply.github.com>
2023-03-28 20:29:04 +0200

    added video

--- src/js/how-its-made.js
+++ src/js/how-its-made.js
@@ -2,6 +2,13 @@ window.addEventListener('DOMContentLoaded', function () {
   var video = document.querySelector('.video');
 
   video.addEventListener('click', function () {
-    alert();
+    if (video.classList.contains('ready')) {
+      return;
+    }
+    video.classList.add('ready');
+    video.insertAdjacentHTML(
+      'afterbegin',
+      '<iframe src="https://www.youtube.com/embed/EwRl6yaYdPc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    );
   });
 });
